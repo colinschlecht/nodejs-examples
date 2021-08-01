8669f4bfa842c1fbb90f879905d1f269015435d7
Colin Schlecht <schlechtcolin@yahoo.com>
2021-08-01 09:57:40 -0700

    moves test file and adds more 'content'

--- streams/streams.js
+++ streams/streams.js
@@ -1,1 +1,17 @@
 import fs from "fs";
+import http from "http";
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+	// solution 1
+	fs.readFile("test-file.txt", (err, data) => {
+		if (err) console.log(err);
+        res.end(data)
+	});
+});
+
+
+server.listen(8000, "127.0.0.1", () => {
+	console.log("waiting for requests");
+});

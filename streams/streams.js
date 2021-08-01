c08ea7e348b67816d08f605ac93d818837f2aabb
Colin Schlecht <schlechtcolin@yahoo.com>
2021-08-01 10:03:49 -0700

    adds readable stream in comparison to readfile

--- streams/streams.js
+++ streams/streams.js
@@ -4,13 +4,18 @@ import http from "http";
 const server = http.createServer();
 
 server.on("request", (req, res) => {
-	// solution 1
-	fs.readFile("test-file.txt", (err, data) => {
-		if (err) console.log(err);
-        res.end(data)
+	// solution 1 - not streams
+	// fs.readFile("test-file.txt", (err, data) => {
+	// 	if (err) console.log(err);
+	//     res.end(data)
+	// });
+
+    // solution 2 - not streams
+	const readable = fs.createReadStream("test-file.txt");
+	readable.on("data", (chunk) => {
+		res.write(chunk);
 	});
 });
-
 
 server.listen(8000, "127.0.0.1", () => {
 	console.log("waiting for requests");
